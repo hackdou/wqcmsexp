27a83c5483312a41da57fdbcad399f0099930c8c
hackdou <2734750265@qq.com>
2017-02-24 13:43:50 +0800

    Create 结果检测.py

---
+++ 结果检测.py
@@ -0,0 +1,40 @@
+import os,urllib2,time
+
+jieguo=[]
+def saveListToFile(file,list):
+    """
+        将列表逐行写如文件中
+    :return:
+    """
+    s = '\n'.join(list)
+    with open(file,'a') as output:
+        output.write(s)
+def zhengli():
+    fp=open("jieguo.txt", "r")
+    alllines=fp.readlines()
+    fp.close()
+    for eachline in alllines:
+        eachline=eachline.strip('\n')
+        eachline=eachline.strip(' ')
+        http_url=eachline
+        try:
+            req=urllib2.Request(http_url)
+            req.add_header('User-Agent','Mozilla/5.0')
+            resp = urllib2.urlopen(req, timeout=10)
+            qrcont=resp.read()
+     
+        except Exception,e:
+            a = str(e)
+            if 'HTTP Error 500' in a:
+                print http_url
+                jieguo.append(http_url)
+            else:
+                print 'not shell'
+        
+def main():
+    zhengli()
+    saveListToFile('shell.txt',jieguo)
+  
+if __name__ == '__main__':
+    main()
+        
